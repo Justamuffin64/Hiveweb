27b44d6ffb259939426bb2976d3920088d80a1da
JustAMuffin <axel.d.senders@gmail.com>
2026-02-25 14:51:54 -0800

    Data can be sent between server and client

--- hiveweb.py
+++ hiveweb.py
@@ -5,6 +5,8 @@ PORT = 8330
 HOST = 'localhost'
 END = b'<END>'
 
+
+
 class _CommunicatingObject(object):
     def __init__(self,PORT):
         """
@@ -16,14 +18,6 @@ class _CommunicatingObject(object):
         #create socket
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        #bind to all interfaces
-        self.s.bind(('',PORT))
-        #listen to all interfaces
-        self.s.listen()
-
-        #declare epmty list of connections
-        self.connections = []
-
         ###ADD CONNECTION CODE DEPENDING ON IF SERVER OR CLASS
 
     def receive(self,data):
@@ -31,18 +25,10 @@ class _CommunicatingObject(object):
         Called whenever the server receives data!
         """
         pass #Implement in extended class!
-
-    def send(self,IP,data):
-        """
-        Call to send data to target IP.
-        """
-        self.se = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.se.connect((IP, PORT))
-        self.se.send(data)
         
 
 #USED IN LISTENING THREAD
-def listen_thread(connection, server):
+def _listen_thread(connection, instance):
     """
     Indefinetly listens for input.
     """
@@ -51,28 +37,50 @@ def listen_thread(connection, server):
         while data[-5:] != END:
             data += connection.recv(1024)
         data = data[:-5].decode()
-        server.receive(data)
+        instance.receive(data)
 
 
 class Server(_CommunicatingObject):
+    def __init__(self, PORT):
+        super().__init__(PORT)
+        self.members = {}
+        #bind to all interfaces
+        self.s.bind(('',PORT))
+        #listen to all interfaces
+        self.s.listen()
+    
     def start(self):
         while True:
             #connect to the client
             #connection is the connection, address is their IP I think.
             self.connection, self.address = self.s.accept()
-            with self.connection:
-                #create a thread for each user to receive data
-                self.connections.append(connection)
-                t=threading.Thread(target=listen_thread, args=(connection,self))
-                t.start()
+            self.members[self.address] = self.connection
+            #send adress to client on connection
+            self.send(self.connection,repr(self.address))
+            #create a thread for each user to receive data
+            self.t=threading.Thread(target=_listen_thread, args=(self.connection,self,))
+            self.t.start()
+
+    def send(self,con,data):
+        """
+        Call to send data to connection ('con').
+        """
+        con.send(data.encode()+b'<END>')
+
 
 class Client(_CommunicatingObject):
-    def start(self):
-        #connect to the server
-        #connection is the connection, address is their IP I think.
-        self.connection, self.address = self.s.accept()
-        #add server to connections
-        self.connections.append(connection)
-        #create a thread to read server data
-        t=threading.Thread(target=listen_thread, args=(connection,self))
-        t.start()
+    def __init__(self, PORT, IP):
+        super().__init__(PORT)
+        #connect to server
+        self.s.connect((IP, PORT))
+        #set self.address to address assigned by server
+        self.address = self.s.recv(1024)[:-5].decode()
+        #start listening for return information
+        self.t=threading.Thread(target=_listen_thread, args=(self.s,self))
+        self.t.start()
+
+    def send(self,data):
+        """
+        Call to send data to target IP.
+        """
+        self.s.send(data.encode()+b'<END>')
---
+++ testing.py
@@ -0,0 +1,23 @@
+import hiveweb
+import threading
+
+class Server(hiveweb.Server):
+    def receive(self, data):
+        print('S: '+data)
+
+class Client(hiveweb.Client):
+    def __init__(self, PORT, IP):
+        super().__init__(PORT, IP)
+        
+    
+    def receive(self, data):
+        print('C: '+data)
+
+
+
+server = Server(8330)
+th=threading.Thread(target=server.start).start()
+
+me = Client(8330,'localhost')
+
+
