ffc4b5f669f4eb6363dba4b104b1d426fe3b7981
JustAMuffin <axel.d.senders@gmail.com>
2026-02-25 21:05:12 -0800

    Updated test file to test multiple clients simultaneously

--- testing.py
+++ testing.py
@@ -14,10 +14,19 @@ class Client(hiveweb.Client):
         print('C: '+data)
 
 
+class ClientTwo(hiveweb.Client):
+    def __init__(self, PORT, IP):
+        super().__init__(PORT, IP)
+        
+    
+    def receive(self, data):
+        print('C2: '+data)
+
 #Imagine this is on a seperate device
 server = Server(8330)
 th=threading.Thread(target=server.start).start()
 
 me = Client(8330,'localhost')
+me2 = ClientTwo(8330,'localhost')
 
 
